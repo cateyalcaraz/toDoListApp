a88cbede3482582f8dbb56f2820173e4a2bc56f9
catey <craalcaraz@gmail.com>
2021-06-14 14:50:43 -0500

    print statement complete

---
+++ toDoListApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  toDoListApp
+//
+//  Created by Apple on 6/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("the girls are fighting")
+    }
+
+
+}
+
